508afcd9af9f94bee369fc6f3a5da9d4b88474ba
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-03-18 23:40:07 -0500

    oops

--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const router = express.Router()
-import {getPosts, createPost} from "../controllers/posts.js"
-
-router.get("/", getPosts)
-
-router.post("/post", createPost)
-
-export default router
