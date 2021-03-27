5347fd8919e182f8ebd8cb3d7259f853b5f31950
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-03-27 17:40:55 -0500

    deploy fixes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import "dotenv/config.js";
+// import "dotenv/config.js";
 import express from "express";
 const app = express();
 import cors from "cors";
@@ -6,13 +6,16 @@ app.use(cors());
 import bodyParser from "body-parser";
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-import morgan from "morgan"
-morgan("tiny")
+
 import mongoose from "mongoose";
 
 import postRoutes from "./routes/posts.js";
 
 app.use("/posts", postRoutes);
+
+app.use("/", (req, res) => {
+  res.send("Connected! Go to /posts for the api!")
+});
 
 const PORT = process.env.PORT || 5000;
 
